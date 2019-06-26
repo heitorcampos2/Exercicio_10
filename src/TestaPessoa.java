0bf9211a4cb49769e058f06137bc4b3177bd7078
heitorcampos2 <heitormtb12@gmail.com>
2019-06-26 15:58:30 -0300

    v1

---
+++ src/TestaPessoa.java
@@ -0,0 +1,13 @@
+public class TestaPessoa {
+    public static void main(String[] args) {
+        
+        try{
+            Pessoa p = new Pessoa("Ciclano", -1);
+        }catch(IdadeInvalidaException i){
+            
+            System.out.println(i.getMessage());
+        }
+        
+        System.out.println("...");
+    }    
+}
